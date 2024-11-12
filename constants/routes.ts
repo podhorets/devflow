e15713b29fe03ce41e09bc176a6b48a4c289c737
d.podhorets <d.podhorets@cid.com>
2024-11-12 22:47:16 +0100

    implement MobileNavigation and NavLinks

--- constants/routes.ts
+++ constants/routes.ts
@@ -2,6 +2,12 @@ const ROUTES = {
   HOME: "/",
   SIGN_IN: "/sign-in",
   SIGN_UP: "/sign-up",
+  COMUNITY: "/community",
+  COLLECTION: "/collection",
+  JOBS: "/jobs",
+  TAGS: "/tags",
+  PROFILE: "/profile",
+  ASK_QUESTION: "/ask-question",
 };
 
 export default ROUTES;
