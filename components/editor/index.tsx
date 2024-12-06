2ff66beb83512828aac8f327c6878db1ff03a1d4
d.podhorets <d.podhorets@cid.com>
2024-12-06 19:18:21 +0100

    implement tag input

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -49,7 +49,7 @@ const Editor = ({ value, editorRef, fieldChange, ...props }: Props) => {
       markdown={value}
       ref={editorRef}
       className="background-light800_dark200 light-border-2 markdown-editor dark-editor w-full
-        border"
+        border grid"
       onChange={fieldChange}
       plugins={[
         // Example Plugin Usage
