8d6aabea482dff6fcb32ed90625393ab4974674f
d.podhorets <d.podhorets@cid.com>
2024-12-06 20:11:54 +0100

    setup mongodb and mongoose

---
+++ lib/mongoose.ts
@@ -0,0 +1,49 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI is not defined");
+}
+
+interface MongooseCache {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache;
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = { conn: null, promise: null };
+  global.mongoose = cached;
+}
+
+const dbConnect = async (): Promise<Mongoose> => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        dbName: "devflow",
+      })
+      .then((result) => {
+        console.log("MongoDB connected");
+        return result;
+      })
+      .catch((error) => {
+        console.error("MongoDB connection error", error);
+        throw error;
+      });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
+
+export default dbConnect;
