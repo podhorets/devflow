d8963b4bccdf521def469ae0e2ea2115dcb21dd1
d.podhorets <d.podhorets@cid.com>
2024-12-10 08:06:48 +0100

    implement account model

--- database/user.model.ts
+++ database/user.model.ts
@@ -11,7 +11,7 @@ export interface IUser {
   reputation?: number;
 }
 
-const UserSchema = new Schema(
+const UserSchema = new Schema<IUser>(
   {
     name: {
       type: String,
@@ -35,6 +35,6 @@ const UserSchema = new Schema(
   { timestamps: true },
 );
 
-const User = models?.user || model<IUser>("User", UserSchema);
+const User = models?.User || model<IUser>("User", UserSchema);
 
 export default User;
